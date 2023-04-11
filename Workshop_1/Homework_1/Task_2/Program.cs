dfbc9e8658d5e0e5baf27f6d66102d3e6b8f0985
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-11 09:24:34 +0300

    Task_2

--- Workshop_1/Homework_1/Task_2/Program.cs
+++ Workshop_1/Homework_1/Task_2/Program.cs
@@ -3,14 +3,35 @@
 44 5 78 -> 78
 22 3 9 -> 22*/
 
+// Console.Write("Введи первое число ");
+// int num_1 = int.Parse(Console.ReadLine());
+
+// Console.Write("Введи второе число ");
+// int num_2 = int.Parse(Console.ReadLine());
+
+// Console.Write("Введи третье число ");
+// int num_3 = int.Parse(Console.ReadLine());
+
+// int maxNum = num_1;
+
+// if (num_2 > maxNum) {
+//     maxNum = num_2;
+// }
+
+// if (num_3 > maxNum) {
+//     maxNum = num_3;
+// }
+
+// Console.WriteLine("max = " + maxNum);
+
 Console.Write("Введи первое число ");
-int num_1 = int.Parse(Console.ReadLine());
+int num_1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введи второе число ");
-int num_2 = int.Parse(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введи третье число ");
-int num_3 = int.Parse(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
 
 int maxNum = num_1;
 
