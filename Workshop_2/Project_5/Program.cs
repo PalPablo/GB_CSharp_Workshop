e35dfa780e4f68d42bc84362276d42957759aa93
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-11 20:35:53 +0300

    Project_5

--- Workshop_2/Project_5/Program.cs
+++ Workshop_2/Project_5/Program.cs
@@ -6,23 +6,35 @@
 // ● 25, 5 -> да
 // ● 8,9 -> нет
 
-Console.Write("Введи первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+// Console.Write("Введи первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
 
-Console.Write("Введи второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+// Console.Write("Введи второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
 
-if (num1 * num1 == num2)
-{
-     Console.Write("да");
+// if (num1 * num1 == num2)
+// {
+//      Console.Write("да");
+// }
+
+// else if (num2 * num2 == num1)
+// {
+//     Console.Write("да");
+// }
+
+// else 
+// {
+//     Console.Write("нет");
+// }
+
+Console.WriteLine("введите число 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("введите число 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 * number1 == number2  || number2 * number2 == number1) {
+Console.WriteLine("да");
 }
-
-else if (num2 * num2 == num1)
-{
-    Console.Write("да");
-}
-
-else 
-{
-    Console.Write("нет");
+else {
+Console.WriteLine("нет");
 }
