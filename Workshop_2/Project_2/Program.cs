166e072c3fe3ee7f74da50ea3aec19ad0cd3ba67
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-11 20:21:51 +0300

    Project_2

--- Workshop_2/Project_2/Program.cs
+++ Workshop_2/Project_2/Program.cs
@@ -12,4 +12,14 @@ int result = (n / 100 * 10) + (n % 10);
 
 Console.WriteLine(result);
 
+// int number = new Random().Next(100, 1000);
+// int number1 = number / 100;
+// int number2 = number % 100 / 10; 
+// int number3 = number % 10;
+// Console.WriteLine($"{number1}"); 
+// Console.WriteLine($"{number2}"); 
+// Console.WriteLine($"{number3}"); 
+// Console.WriteLine($"{number}"); 
+// Console.WriteLine($"{number1 * 10 + number3}");
+
 
