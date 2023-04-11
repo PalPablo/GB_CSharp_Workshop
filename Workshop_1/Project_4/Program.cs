1db6348c54d85549e738620b954161ac74e970e2
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-11 09:18:42 +0300

    Project_4

--- Workshop_1/Project_4/Program.cs
+++ Workshop_1/Project_4/Program.cs
@@ -5,8 +5,15 @@
 782 -> 2
 918 -> 8*/
 
+// Console.Write("Введи трехзначное число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int result = number % 10;
+
+// Console.Write(result);
+
 Console.Write("Введи трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 int result = number % 10;
 
