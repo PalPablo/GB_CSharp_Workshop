419a10b0f386f423168d16fbd69b101b76996ed2
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-20 22:06:14 +0300

    Project_2

---
+++ Workshop_3/Project_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти  ");
+int N4 = Convert.ToInt32(Console.ReadLine());
+
+if(N4 == 1)
+{
+  Console.WriteLine ("X>0, Y>0");
+}
+else if (N4 == 2)
+{
+  Console.Write("X<0, Y>0");
+}
+else if (N4 == 3)
+{
+  Console.Write("X<0, Y<0");
+}
+else if (N4 == 4)
+{
+  Console.Write("X>0, Y<0");
+}
+else if (N4 > 4 || N4 < 1)
+{
+  Console.Write("такой четверти нет");
+}
+
