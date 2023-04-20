326d5e0ce9d9d9858d2792aae114dabd8e116e64
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-20 07:24:22 +0300

    Task_4

--- Workshop_1/Homework_1/Task_4/Program.cs
+++ Workshop_1/Homework_1/Task_4/Program.cs
@@ -3,9 +3,36 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8*/
 
-Console.WriteLine("Введите число: ");
-int N = int.Parse(Console.ReadLine());
-    
+// Console.Write("Введи число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Все четные числа от 1 до {N}: ");
+
+// int num = 1;
+// while (num <= N)
+// {
+//     if (num % 2 == 0)
+//     {
+//         Console.WriteLine(num);
+//     }
+//     num++;
+//     }
+
+
+Console.Write("Введи число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Все четные числа от 1 до {N}:");
+
 for (int i = 1; i <= N; i++)
-    if (i % 2 == 0)
-        Console.WriteLine(i);
+    if (i % 2 == 0) 
+Console.WriteLine(i);
+
+
+
+// Console.WriteLine("Введите число: ");
+// int N = int.Parse(Console.ReadLine());
+
+// for (int i = 1; i <= N; i++)
+//     if (i % 2 == 0)
+//         Console.WriteLine(i);
