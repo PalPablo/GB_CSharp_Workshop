dbe6e3b2e4bec97bfd05f7d67057387a4e715616
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-25 06:43:36 +0300

    Project_3

---
+++ Workshop_4/Project_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int lenth = 1;
+while (num > 9)
+{
+    num = num / 10;
+    lenth++;
+}
+Console.WriteLine($"Ответ: {lenth}");
---
+++ Workshop_4/Project_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Enter number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int multip = 1;
+for (int i = 1; i <= N; i++)
+{
+    multip = multip * i;
+}
+
+Console.WriteLine($"Multiplication = {multip}");
