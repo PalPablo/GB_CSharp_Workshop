4f376be07bcb0df2194b0e3ef0afd11dc447964a
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-25 06:24:57 +0300

    intro

---
+++ Workshop_4/introduction/Program.cs
@@ -0,0 +1,46 @@
+﻿// Функции
+
+// void SayLang(string lang)
+// {
+//     Console.WriteLine($"Привет {lang}!!!");
+// }
+
+// SayLang("Python");
+// Console.WriteLine ("1");
+// SayLang("Java");
+// Console.WriteLine ("2");
+// string lang1 = "C++";
+// SayLang(lang1);
+
+
+// void SayLang(int num1, int num2)
+// {
+//     if (num1 > num2)
+//         Console.WriteLine($"Привет {num1}!!!");
+//     else
+//         Console.WriteLine($"Привет {num2}!!!");
+// }
+
+// SayLang(1, 2);
+// Console.WriteLine("1");
+// SayLang(3, 1);
+// Console.WriteLine("2");
+
+// SayLang(7, 5);
+
+
+int SayLang(int num1, int num2)
+{
+    if (num1 > num2)  
+        return num1;
+    else
+        return num2;
+}
+
+Console.WriteLine(SayLang(1, 2));
+
+int x = SayLang(3, 1);
+Console.WriteLine ($"Привет, {x}!!!");  
+
+int y = SayLang(7, 5) * 10;
+Console.WriteLine ($"Привет, {y}!!!");
