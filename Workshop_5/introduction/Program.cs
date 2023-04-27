cefe33a9a33adfe021da3da6273f2d1044c110b6
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-27 21:35:34 +0300

    intro

---
+++ Workshop_5/introduction/Program.cs
@@ -0,0 +1,13 @@
+﻿string s = "1, 2, 3, 4, 5, 6, 7.";
+Console.WriteLine(s);
+
+string[] nums = s.Split(new char[]{' ', '.', ','}, StringSplitOptions.RemoveEmptyEntries);
+
+int[] n = nums.Select(Int32.Parse).ToArray();
+
+foreach (var num in nums)
+{
+   Console.WriteLine($" Значение {num}");
+}
+
+int[] num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
