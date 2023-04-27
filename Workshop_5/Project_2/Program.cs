b51e9f0cef63eceda9e74ae8ff819cf13f37c2da
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-27 21:59:54 +0300

    Project_2

---
+++ Workshop_5/Project_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.Clear();
+int[] array = new int[12];
+int result = 0;
+int resultneg = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(-9, 10);
+  Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i]  * (-1);
+    Console.Write($"{array[i]} ");
+}
+
