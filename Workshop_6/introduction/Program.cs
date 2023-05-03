859a222602d90784a89f2b4aadcd38879a0a78d1
Pavel Zykin <prime.87.pz@gmail.com>
2023-05-03 22:52:33 +0300

    introduction

---
+++ Workshop_6/introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 3, 7};
+// двумерный массив - одна запятая, трехмерный - две и т.д.
+int[,] nums = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 0, 3, 7}};
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write($"{nums[i,j]} ");
+    }
+    Console.WriteLine();
+}
