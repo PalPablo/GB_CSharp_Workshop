54e489138cbd011863dd609f0025887ae275da58
Pavel Zykin <prime.87.pz@gmail.com>
2023-05-14 20:12:22 +0300

    homework_6

--- Workshop_6_renew/Homework_6/Program.cs
+++ Workshop_6_renew/Homework_6/Program.cs
@@ -1,3 +1,50 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+// 1, -7, 567, 89, 223-> 4
+
+// Console.Write("Введите количество чисел: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// int countPositive = 0;
+// for (int i = 1; i <= m; i++)
+// {
+//     Console.Write($"Введите число {i}: ");
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     if (number > 0)
+//     {
+//         countPositive++;
+//     }
+// }
+
+// Console.WriteLine($"Количество положительных чисел: {countPositive}");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения: ({x}, {y})");
+    }
+}
