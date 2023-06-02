863940e4170f56e93cf6e7d052df72ba5d880ee6
Pavel Zykin <prime.87.pz@gmail.com>
2023-06-02 08:24:49 +0300

    Homework_8

--- Workshop_8/Homework_8/Program.cs
+++ Workshop_8/Homework_8/Program.cs
@@ -9,50 +9,120 @@
 // 8 4 4 2
 
 
-        int[,] array = new int[,]
-        {
-            { 1, 4, 7, 2 },
-            { 5, 9, 2, 3 },
-            { 8, 4, 2, 4 }
-        };
+// int[,] array = new int[,]
+// {
+//     { 1, 4, 7, 2 },
+//     { 5, 9, 2, 3 },
+//     { 8, 4, 2, 4 }
+// };
 
-        SortRowsDescending(array);
+// SortRowsDescending(array);
 
-        // Вывод отсортированного массива
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                Console.Write(array[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-    
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
 
-    static void SortRowsDescending(int[,] array)
-    {
-        int rowCount = array.GetLength(0);
+// static void SortRowsDescending(int[,] array)
+// {
+//     int rowCount = array.GetLength(0);
 
-        // Сортируем каждую строку массива по убыванию
-        for (int i = 0; i < rowCount; i++)
-        {
-            List<int> tempList = new List<int>();
+//     for (int i = 0; i < rowCount; i++)
+//     {
+//         List<int> tempList = new List<int>();
 
-            // Добавляем элементы строки во временный список
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                tempList.Add(array[i, j]);
-            }
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             tempList.Add(array[i, j]);
+//         }
 
-            // Сортируем временный список по убыванию
-            tempList.Sort((x, y) => y.CompareTo(x));
+//         tempList.Sort((x, y) => y.CompareTo(x));
 
-            // Копируем отсортированные элементы обратно в строку массива
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = tempList[j];
-            }
-        }
-    }
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = tempList[j];
+//         }
+//     }
+// }
 
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+//     int[,] array = new int[,]
+//     {
+//         { 1, 4, 7, 2 },
+//         { 5, 9, 2, 3 },
+//         { 8, 4, 2, 4 },
+//         { 5, 2, 6, 7 }
+//     };
+
+//     Console.WriteLine("Исходный массив:");
+//     PrintArray(array);
+
+//     int minRowIndex = FindRowWithMinimumSum(array);
+
+//     Console.WriteLine($"Строка с наименьшей суммой элементов: {minRowIndex + 1}");
+
+
+// static int FindRowWithMinimumSum(int[,] array)
+// {
+//     int rowCount = array.GetLength(0);
+//     int columnCount = array.GetLength(1);
+
+//     int[] rowSums = new int[rowCount];
+
+
+//     for (int i = 0; i < rowCount; i++)
+//     {
+//         int rowSum = 0;
+
+//         for (int j = 0; j < columnCount; j++)
+//         {
+//             rowSum += array[i, j];
+//         }
+
+//         rowSums[i] = rowSum;
+//     }
+
+//     int minRowIndex = 0;
+//     int minRowSum = rowSums[0];
+
+//     for (int i = 1; i < rowCount; i++)
+//     {
+//         if (rowSums[i] < minRowSum)
+//         {
+//             minRowIndex = i;
+//             minRowSum = rowSums[i];
+//         }
+//     }
+
+//     return minRowIndex;
+// }
+
+// static void PrintArray(int[,] array)
+// {
+//     int rowCount = array.GetLength(0);
+//     int columnCount = array.GetLength(1);
+
+//     for (int i = 0; i < rowCount; i++)
+//     {
+//         for (int j = 0; j < columnCount; j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
